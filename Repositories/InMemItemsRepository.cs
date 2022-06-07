7321ab7ae0b0a9e0c4fe0b7fa12ff0683fb8657c
ProbstenHias <matthias.weilinger@gmx.de>
2022-06-07 09:14:49 +0200

    changes

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using TodoApi.Entities;
+
+namespace TodoApi.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item {Id=Guid.NewGuid(), Name= "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow},
+            new Item {Id=Guid.NewGuid(), Name= "Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow},
+            new Item {Id=Guid.NewGuid(), Name= "Shield", Price = 10, CreatedDate = DateTimeOffset.UtcNow}
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item? GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
